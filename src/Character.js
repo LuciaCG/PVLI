2a046bf5a424e3e6d58c45d656f4907cee5803e9
LucíaCG <luciacarr_97@hotmail.com>
2016-11-08 19:45:11 +0100

    primeros tests pasados, hay que hacer a partir de SCROLL

---
+++ src/Character.js
@@ -0,0 +1,75 @@
+'use strict';
+var dice = require('./dice');
+
+function Character(name, features) {
+  features = features || {};
+  this.name = name;
+  // Extrae del parámetro features cada característica y alamacénala en
+  // una propiedad.
+  this.party = features.party || null;
+  this.initiative = features.initiative || 0;
+  this.defense = features.defense || 0;
+  this.weapon = features.weapon || null;
+  this._mp = features.mp || 0;
+  this._hp = features.hp || 0;
+  this.maxMp = features.maxMp || this._mp || 0;
+  this.maxHp = features.maxHp || this._hp || 15;
+}
+
+Character.prototype._immuneToEffect = ['name', 'weapon'];
+
+Character.prototype.isDead = function () {
+	if(this._hp <= 0) return true;
+	
+	else return false;
+};
+
+Character.prototype.applyEffect = function (effect, isAlly) {
+  // Implementa las reglas de aplicación de efecto para modificar las
+  // características del personaje. Recuerda devolver true o false según
+  // si el efecto se ha aplicado o no.
+  if (!isAlly)
+    if (dice.d100() <= this.defense)
+        return false;
+
+  this.initiative += effect.initiative;
+  this.defense += effect.defense;
+  this._mp += effect.mp;
+  this._hp += effect.hp;
+  this.maxMp += effect.maxMp;
+  this.maxHp += effect.maxHp;
+
+  return true;
+
+};
+
+Object.defineProperty(Character.prototype, 'mp', {
+  get: function () {
+    return this._mp;
+  },
+  set: function (newValue) {
+    this._mp = Math.max(0, Math.min(newValue, this.maxMp));
+  }
+});
+
+Object.defineProperty(Character.prototype, 'hp', {
+  // Puedes usar la mísma ténica que antes para mantener el valor de hp en el
+  // rango correcto.
+  get: function () {
+    return this._hp;
+  },
+  set: function (newValue) {
+    this._hp = Math.max(0, Math.min(newValue, this.maxHp));
+  }
+});
+
+Object.defineProperty(Character.prototype, 'defense', {
+  get: function () {
+    return this._defense;
+  },
+  set: function (newValue) {
+    this._defense = Math.max(0, Math.min(newValue, 100));
+  }
+});
+
+module.exports = Character;
---
+++ src/items.js
@@ -0,0 +1,53 @@
+'use strict';
+
+function Item(name, effect) {
+  this.name = name;
+  this.effect = effect;
+}
+
+function Weapon(name, damage, extraEffect) {
+  extraEffect = extraEffect || new Effect({});
+  // Haz que Weapon sea subtipo de Item haciendo que llame al constructor de
+  // de Item.
+  extraEffect.hp += damage*(-1);
+  Item.call(this, name, extraEffect);
+}
+// Termina de implementar la herencia haciendo que la propiedad prototype de
+// Item sea el prototipo de Weapon.prototype y recuerda ajustar el constructor.
+Weapon.prototype = Object.create(Item.prototype);
+Weapon.prototype.constructor = Weapon;
+
+function Scroll(name, cost, effect) {
+  Item.call(this, name, effect);
+  this.cost = cost;
+}
+Scroll.prototype = Object.create(Item.prototype);
+Scroll.prototype.constructor = Scroll;
+
+Scroll.prototype.canBeUsed = function (mp) {
+  // El pergamino puede usarse si los puntos de maná son superiores o iguales
+  // al coste del hechizo.
+  if ()
+
+
+};
+
+function Effect(variations) {
+  // Copia las propiedades que se encuentran en variations como propiedades de
+  // este objeto.
+  this.initiative = variations.initiative || 0;
+  this.defense = variations.defense || 0;
+  this.hp = variations.hp || 0;
+  this.maxHp = variations.maxHp || 0;
+  this.mp = variations.mp || 0;
+  this.maxMp = variations.maxHp || 0;
+
+} 
+ 
+
+module.exports = {
+  Item: Item,
+  Weapon: Weapon,
+  Scroll: Scroll,
+  Effect: Effect
+};
